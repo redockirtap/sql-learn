e5942ef1d1c821e93cf418d52283b341e6ec67a3
redockirtap <redockirtap@gmail.com>
2023-03-10 14:17:32 +0200

    Inner Select

--- learn.sql
+++ learn.sql
@@ -155,3 +155,51 @@ SELECT winner, subject
 FROM nobel
 WHERE yr=1984
 ORDER BY subject IN ('physics','chemistry'), subject, winner
+
+
+SELECT name FROM world
+WHERE population >
+(SELECT population FROM world
+WHERE name='Russia')
+
+SELECT name FROM world
+WHERE gdp/population >
+(SELECT gdp/population FROM world
+WHERE name = 'United Kingdom') AND continent = 'Europe'
+
+SELECT name, continent FROM world
+  WHERE continent IN (SELECT continent FROM world
+      WHERE name = 'Argentina' OR name = 'Australia')
+ORDER BY name
+
+SELECT name, population FROM world
+  WHERE population > (SELECT population FROM world
+      WHERE name = 'United Kingdom') AND population < (SELECT population FROM world
+      WHERE name = 'Germany')
+
+
+SELECT name, CONCAT(ROUND(population*100/(SELECT population FROM world WHERE name = 'Germany')), '%') FROM world 
+WHERE continent = 'Europe'
+
+
+SELECT name FROM world
+ WHERE gdp > ALL(SELECT gdp FROM world
+WHERE continent = 'Europe' AND gdp > 0)
+
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent)
+
+
+SELECT name, continent, population FROM world x
+  WHERE 25000000>=ALL (SELECT population FROM world y WHERE x.continent=y.continent)
+
+
+
+SELECT name, continent FROM world x
+  WHERE population/3 >= ALL (SELECT population FROM world y WHERE x.continent=y.continent AND x.name != y.name)
+
+
+  
